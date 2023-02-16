28b80282917dbd40189af708c87fbadbebafb5a3
wsk891213 <wsk891213@gmail.com>
2023-02-16 11:15:52 +0900

    문제 해결

--- 옹알이 (1).js
+++ 옹알이 (1).js
@@ -1,8 +1,14 @@
 function solution(babbling) {
-    var answer = 10;
-    
-    
+    var answer = 0;
+    var regExp = /^(aya|ye|woo|ma)+$/;
 
+    babbling.forEach(element => {
+        if(regExp.test(element)) answer++
+    });
 
     return answer;
 }
+
+var babbling = ["ayaye", "uuuma", "ye", "yemawoo", "ayaa"];
+
+console.log(solution(babbling));
